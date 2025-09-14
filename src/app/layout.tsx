9e71291c4093f5c25525a6d03e19522d5d935576
revelsprk <revelsprk@gmail.com>
2025-09-14 16:27:30 +0900

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "DoJang",
   description: "Your ultimate task master!",
+  openGraph: {
+    title: "DoJang",
+    description: "Your ultimate task master!",
+    url: "https://do-jang.vercel.app",
+    siteName: "DoJang",
+    images: [
+      {
+        url: "https://yourdomain.com/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "DoJang - Your ultimate task master!",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-}
