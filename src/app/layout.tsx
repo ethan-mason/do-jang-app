2089b216f8771db401336b55d28e0669bc1ab80c
revelsprk <revelsprk@gmail.com>
2025-09-14 16:28:29 +0900

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     siteName: "DoJang",
     images: [
       {
-        url: "https://yourdomain.com/og-image.png",
+        url: "https://raw.githubusercontent.com/ethan-mason/claro-list/refs/heads/main/public/og-image.png",
         width: 1200,
         height: 630,
         alt: "DoJang - Your ultimate task master!",
