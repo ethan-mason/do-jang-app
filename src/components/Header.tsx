28192d0e8c941bc14f558de37c5ec43c017d8864
revelsprk <revelsprk@gmail.com>
2025-10-11 22:04:52 +0900

    Add a user function

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import Button from "./ui/Button";
 
 export default function Header() {
     return (
-        <div className="flex sticky top-0 z-50 px-4 md:px-0 h-20 flex items-center bg-white">
-            <div className="md:max-w-md w-full md:mx-auto">
-            <Link href="/" className="flex items-center w-fit">
-            <Image src="/logo.svg" alt="Logo" width={100} height={100} className="w-8 select-none" />
-            <h1 className="font-bold text-2xl ml-2 header-logo text-black">DoJang</h1>
-            </Link>
+        <div className="flex sticky top-0 z-50 px-4 md:px-0 h-20 bg-white">
+            <div className="md:max-w-md w-full md:mx-auto flex items-center">
+                <Link href="/" className="flex items-center w-fit">
+                    <Image src="/logo.svg" alt="Logo" width={100} height={100} className="w-8 select-none" />
+                    <h1 className="font-bold text-2xl ml-2 header-logo text-black">DoJang</h1>
+                </Link>
+                <div className="ml-auto">
+                    <Button size="sm">Login</Button>
+                </div>
             </div>
         </div>
     )
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,6 +5,7 @@ import classNames from "classnames";
 type ButtonProps = {
     children: ReactNode;
     variant?: "primary" | "secondary";
+    size?: "sm" | "md";
     icon?: ReactNode;
     className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
@@ -12,16 +13,20 @@ type ButtonProps = {
 export default function Button({
     children,
     variant = "primary",
+    size = "md",
     icon,
     className,
     ...props
-    }: ButtonProps) {
+}: ButtonProps) {
     return (
         <button
         {...props}
         className={classNames(
-            "select-none px-4 py-2 rounded-full font-semibold outline-none focus-visible:ring-2 ring-slate-300 ring-offset-2 duration-200 disabled:opacity-50 flex items-center justify-center",
+            "select-none rounded-full font-semibold outline-none focus-visible:ring-2 ring-slate-300 ring-offset-2 duration-200 disabled:opacity-50 flex items-center justify-center",
             {
+            "px-3.5 py-1.5 text-sm": size === "sm",
+            "px-4 py-2 text-base": size === "md",
+
             "bg-slate-900 text-white hover:bg-slate-700": variant === "primary",
             "bg-white border border-slate-200 hover:bg-slate-100 text-slate-900":
                 variant === "secondary",
