6f0c65798993612c3b5f7798397da0ccafaae431
revelsprk <revelsprk@gmail.com>
2025-10-12 22:03:58 +0900

    Update

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,6 @@ import classNames from "classnames";
 type ButtonProps = {
     children: ReactNode;
     variant?: "primary" | "secondary";
-    size?: "sm" | "md";
     icon?: ReactNode;
     className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
@@ -13,7 +12,6 @@ type ButtonProps = {
 export default function Button({
     children,
     variant = "primary",
-    size = "md",
     icon,
     className,
     ...props
@@ -22,11 +20,8 @@ export default function Button({
         <button
         {...props}
         className={classNames(
-            "select-none rounded-full font-semibold outline-none focus-visible:ring-2 ring-slate-300 ring-offset-2 duration-200 disabled:opacity-50 flex items-center justify-center",
+            "select-none rounded-full font-semibold outline-none focus-visible:ring-2 ring-slate-300 ring-offset-2 duration-200 disabled:opacity-50 flex items-center justify-center px-4 py-2 text-base",
             {
-            "px-3.5 py-1.5 text-sm": size === "sm",
-            "px-4 py-2 text-base": size === "md",
-
             "bg-slate-900 text-white hover:bg-slate-700": variant === "primary",
             "bg-white border border-slate-200 hover:bg-slate-100 text-slate-900":
                 variant === "secondary",
